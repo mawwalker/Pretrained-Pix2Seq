df70d99e34f1ab0e2f585e2dd16259fdf3202859
gentlemanarch <dengshima@gmail.com>
2022-03-01 10:05:51 +0800

    DOTA脚本

--- read_weights.py
+++ read_weights.py
@@ -1,11 +1,8 @@
-from re import M
 import torch
 
-# pretrained_weights = torch.load('./coco_ap370.pth')
+pretrained_weights = torch.load('./coco_ap370.pth')
 
-pretrained_weights = torch.load('output/ships_v3_L/checkpoint_best.pth', map_location='cpu')
-
-num_classes = 12 + 1
+num_classes = 15 + 1
 num_bins = 2000
 num_vocal = num_bins + 1 + num_classes + 2
 '''
@@ -18,30 +15,23 @@ transformer.vocal_classifier.bias: torch.Size([2094])
 transformer.det_embed.weight: torch.Size([1, 256])
 transformer.vocal_embed.weight: torch.Size([2092, 256])
 '''
-
+# for x in pretrained['model']:
+    # print(x)
 model = pretrained_weights['model']
-# print(model)
-
-# print(pretrained_weights.keys())
-# print(pretrained_weights['counts'])
-
 for x in model:
     if isinstance(model[x], torch.Tensor):
         print('{}: {}'.format(x, model[x].shape))
 
 # pretrained_weights["model"]["class_embed.weight"].resize_(num_class+1,256)
 # pretrained_weights["model"]["class_embed.bias"].resize_(num_class+1)
+pretrained_weights['model']['transformer.vocal_classifier.weight'].resize_(num_vocal, 256)
+pretrained_weights['model']['transformer.vocal_classifier.bias'].resize_(num_vocal)
+pretrained_weights['model']['transformer.vocal_embed.weight'].resize_(num_bins + num_classes + 1, 256)
 
-# resize the class shape
-# pretrained_weights['model']['transformer.vocal_classifier.weight'].resize_(num_vocal, 256)
-# pretrained_weights['model']['transformer.vocal_classifier.bias'].resize_(num_vocal)
-# pretrained_weights['model']['transformer.vocal_embed.weight'].resize_(num_bins + num_classes + 1, 256)
+for x in pretrained_weights['model']:
+    if isinstance(model[x], torch.Tensor):
+        print('{}: {}'.format(x, model[x].shape))
 
-# for x in pretrained_weights['model']:
-#     if isinstance(model[x], torch.Tensor):
-#         print('{}: {}'.format(x, model[x].shape))
-
-# torch.save(pretrained_weights,'pix2seq_r50_%d.pth'%num_classes)
-
+torch.save(pretrained_weights,'pix2seq_r50_%d.pth'%num_classes)
 # print('input_proj.0.weight: {}, input_proj.1.weight: {}'.format(model["input_proj.0.weight"].shape,
 #                                                                 model["input_proj.1.weight"].shape))
