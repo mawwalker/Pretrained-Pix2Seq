20c60d6545899283df5e410a06b3dd83aee5111c
dengshima <dengshima@gmail.com>
2022-03-02 09:44:03 +0800

    deformable transformer add

--- train_dota.sh
+++ train_dota.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py --coco_path ./DOTA --num_classes 15 --pix2seq_lr --large_scale_jitter --rand_target $@
+python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --coco_path ./DOTA --num_classes 15 --pix2seq_lr --large_scale_jitter --rand_target $@
