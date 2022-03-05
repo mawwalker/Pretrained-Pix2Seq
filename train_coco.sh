dcac8457a020396e2faaa7f3878a1358b6cbd38f
dengshima <dengshima@gmail.com>
2022-03-05 16:50:07 +0800

    训练脚本更新

--- train_coco.sh
+++ train_coco.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-
-python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py --coco_path ./coco --num_classes 90 --pix2seq_lr --large_scale_jitter --rand_target $@
+GPUS=$1
+python -m torch.distributed.launch --nproc_per_node=$GPUS --use_env main.py \
+    --pix2seq_lr --large_scale_jitter --rand_target \
+    --model pix2seq \
+    --resume ./coco_ap370.pth \
+    --backbone swin_L \
+    --transfer \
+    --swin_path weights/swin_large_patch4_window7_224_22k.pth \
+    --batch_size 4 \
+    --coco_path ./coco \
+    --num_classes 90 \
+    --output_dir ./output/coco_v5
--- train_ships.sh
+++ train_ships.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-
-python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --coco_path ./ships --num_classes 12 --pix2seq_lr --large_scale_jitter --rand_target $@
+GPUS=$1
+python -m torch.distributed.launch --nproc_per_node=$GPUS --use_env main.py \
+    --pix2seq_lr --large_scale_jitter --rand_target \
+    --model pix2seq \
+    --resume pix2seq_r50_13.pth \
+    --transfer \
+    --backbone swin_L \
+    --swin_path weights/swin_large_patch4_window7_224_22k.pth \
+    --batch_size 4 \
+    --coco_path ./ships \
+    --num_classes 12 \
+    --output_dir ./output/ships_v5
